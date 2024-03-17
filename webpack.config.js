660aa8afb322a6899639eb408798acb8a002f9d2
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2024-03-17 11:52:32 +0200

    Added webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = (env) => {
     return {
@@ -6,8 +8,12 @@ module.exports = (env) => {
         entry: path.resolve(__dirname, 'src', 'index.js'),
         output: {
             path: path.resolve(__dirname, 'build'), // folder to saving compiled file.
-            filename: '[name].js', // the compiled file mame.
+            filename: '[name].[contenthash].js', // the compiled file mame.
             clean: true // flag to remove old compiled file before compiling.
-        }
+        },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+            new webpack.ProgressPlugin(),
+        ]
     }
 }
