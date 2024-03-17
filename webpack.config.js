3e8edae429aedba1c9cb68bfb6bd2eac8a23be79
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2024-03-17 12:10:22 +0200

    Added TS loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 module.exports = (env) => {
     return {
         mode: env.mode ?? 'development', // mode of build development(don't not optimized)/production (optimized)
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
         output: {
             path: path.resolve(__dirname, 'build'), // folder to saving compiled file.
             filename: '[name].[contenthash].js', // the compiled file mame.
@@ -14,6 +14,18 @@ module.exports = (env) => {
         plugins: [
             new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
             new webpack.ProgressPlugin(),
-        ]
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/, // regular expression.
+                    use: 'ts-loader', // the loader.
+                    exclude: /node_modules/, // won't be processed.
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'], //extension of source code files for loader processing.
+        },
     }
 }
