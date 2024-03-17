90844abf09c3af81af3e422f9792aa96c0928da1
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2024-03-17 11:32:32 +0200

    Added env variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development', // mode of build development(don't not optimized)/production (optimized)
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'), // folder to saving compiled file.
-        filename: '[name].js', // the compiled file mame.
-        clean: true // flag to remove old compiled file before compiling.
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development', // mode of build development(don't not optimized)/production (optimized)
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'build'), // folder to saving compiled file.
+            filename: '[name].js', // the compiled file mame.
+            clean: true // flag to remove old compiled file before compiling.
+        }
     }
 }
