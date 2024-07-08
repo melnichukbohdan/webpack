37d4f1f16789af55f61978c14f441b95200b3d44
Bohdan Melnychuk <b.melnychuk@dev-branch.com>
2024-07-08 17:16:18 +0300

    Assets

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,31 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
+
+    const assetLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    }
+
+      const svgLoader = {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: [{
+              loader: '@svgr/webpack',
+              options: {
+                  icon: true,
+                  svgoConfig: {
+                      plugins: [{
+                          name: 'convertColors',
+                          params: {
+                              currentColor: true,
+                          }
+                      }]
+                  }
+              }
+          }],
+      }
+
     const cssLoaderWithModules = {
             loader: "css-loader",
             options: {
@@ -34,6 +59,8 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
         }
 
     return [
+        assetLoader,
+        svgLoader,
         scssLoader,
         tsLoader
     ]
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,20 +2,32 @@ import React, {useState} from 'react';
 import classes from './App.module.scss';
 import {Link, Outlet} from "react-router-dom";
 import About from "@/pages/About/About";
+import avatarPng from "@/assets/608258.png";
+import avatarJpg from "@/assets/608.jpg";
+import AvatarSvg from "@/assets/app-image.svg";
 
 export const App = () => {
 
     const [count, setCount] = useState(0)
     const increment = () => setCount(prev => prev + 1)
     return (
+      <div>
+        <Link to={'/about'}>About</Link>
+        <br/>
+        <Link to={'/shop'}>Shop</Link>
+        <br/>
+        <h1 className={classes.value}>{count}</h1>
+        <button className={classes.button} onClick={increment}><span>inc</span></button>
+        <About/>
         <div>
-            <Link to={'/about'}>About</Link>
-            <br/>
-            <Link to={'/shop'}>Shop</Link>
-            <br/>
-            <h1 className={classes.value}>{count}</h1>
-            <button className={classes.button} onClick={increment}><span>inc</span></button>
-            <About />
+          <img src={avatarPng} alt="AvatarPng"/>
         </div>
+        <div>
+          <img src={avatarJpg} alt="avatarJpg"/>
+        </div>
+        <div>
+          <AvatarSvg width={500} height={500} style={{color: 'green'}}/>
+        </div>
+      </div>
     );
 };
